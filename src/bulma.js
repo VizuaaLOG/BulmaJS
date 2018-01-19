4b08d5d06401d999e19b83c582b8cb88ba98466c
Thomas Erbe <vizuaalog@gmail.com>
2018-01-19 21:51:09 +0000

    [WIP] Wikiki's accordion extension.

--- src/bulma.js
+++ src/bulma.js
@@ -5,5 +5,6 @@ import Message from './plugins/message';
 import Dropdown from './plugins/dropdown';
 import Modal from './plugins/modal';
 import File from './plugins/file';
+import Accordion from './plugins/accordion';
 
 window.Bulma = Bulma;
