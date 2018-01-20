cc6068243c3ff8774f46d2a25b68212538a96bbf
Thomas Erbe <vizuaalog@gmail.com>
2018-01-20 22:04:28 +0000

    Refactoring and bug fixing

--- src/plugins/calendar.js
+++ src/plugins/calendar.js
@@ -1,5 +1,9 @@
 import Bulma from '../core';
 
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+const shortDays = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+const monthDays = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
 /**
  * @module Calendar
  * @since  0.3.0
@@ -16,109 +20,93 @@ class Calendar {
             throw new Error('[BulmaJS] The Calendar component requires an element.');
         }
 
+        this.now = new Date();
+
+        this.date = options.hasOwnProperty('date') ? options.date : this.now;
+
         /**
          * The root Calendar element.
          * @type {HTMLElement}
          */
         this.root = options.element;
-
         this.root.classList.add('calendar');
-        this.now = new Date();
 
-        this.weekStart = 1;
-        this.months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-        this.shortMonths = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-        this.days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-        this.shortDays = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-        this.monthDays = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-
-        this.year = this.now.getFullYear();
-        this.month = this.now.getMonth();
-        this.day = this.now.getDate();
-
-        this.buildNav();
-        this.buildCalendarContainer();
-        this.buildCalendarHeader();
-        this.buildCalendarBody();
+        this.year = this.date.getFullYear();
+        this.month = this.date.getMonth();
+        this.day = this.date.getDate();
 
         this.render();
     }
 
     buildNav() {
-        this.nav = document.createElement('div');
-        this.nav.classList.add('calendar-nav');
+        let nav = Bulma.createElement('div', 'calendar-nav');
+        let navLeft = Bulma.createElement('div', 'calendar-nav-left');
 
-        let navLeft = document.createElement('div');
-        navLeft.classList.add('calendar-nav-left');
-
-        this.prevMonthButton = document.createElement('button');
-        this.prevMonthButton.classList.add('button', 'is-text');
-        
-        let prevIcon = document.createElement('i');
-        prevIcon.classList.add('fa', 'fa-chevron-left');
-
+        this.prevMonthButton = Bulma.createElement('button', ['button', 'is-text']);
+        let prevIcon = Bulma.createElement('i', ['fa', 'fa-chevron-left']);
         this.prevMonthButton.appendChild(prevIcon);
 
         navLeft.appendChild(this.prevMonthButton);
 
-        let navRight = document.createElement('div');
-        navRight.classList.add('calendar-nav-right');
+        let navRight = Bulma.createElement('div', 'calendar-nav-right');
 
-        this.nextMonthButton = document.createElement('button');
-        this.nextMonthButton.classList.add('button', 'is-text');
-        
-        let nextIcon = document.createElement('i');
-        nextIcon.classList.add('fa', 'fa-chevron-right');
-
+        this.nextMonthButton = Bulma.createElement('button', ['button', 'is-text']);
+        let nextIcon = Bulma.createElement('i', ['fa', 'fa-chevron-right']);
         this.nextMonthButton.appendChild(nextIcon);
 
         navRight.appendChild(this.nextMonthButton);
 
-        this.monthYearLabel = document.createElement('div');
-        this.monthYearLabel.innerHTML = this.months[this.month] + ' ' + this.year;
+        this.monthYearLabel = Bulma.createElement('div');
+        this.monthYearLabel.innerHTML = months[this.month] + ' ' + this.year;
 
-        this.nav.appendChild(navLeft);
-        this.nav.appendChild(this.monthYearLabel);
-        this.nav.appendChild(navRight);
+        nav.appendChild(navLeft);
+        nav.appendChild(this.monthYearLabel);
+        nav.appendChild(navRight);
+
+        return nav;
     }
 
-    buildCalendarContainer() {
-        this.calendarContainer = document.createElement('div');
-        this.calendarContainer.classList.add('calendar-container');
+    buildContainer() {
+        return Bulma.createElement('div', 'calendar-container');
     }
 
-    buildCalendarHeader() {
-        this.calendarHeader = document.createElement('div');
-        this.calendarHeader.classList.add('calendar-header');
+    buildHeader() {
+        let calendarHeader = Bulma.createElement('div', 'calendar-header');
 
-        this.shortDays.forEach((dayName) => {
-            let day = document.createElement('div');
-            day.classList.add('calendar-date');
+        shortDays.forEach((dayName) => {
+            let day = Bulma.createElement('div', 'calendar-date');
             day.innerHTML = dayName;
-            this.calendarHeader.appendChild(day);
+            calendarHeader.appendChild(day);
         });
+
+        return calendarHeader;
     }
 
-    buildCalendarBody() {
-        this.calendarBody = document.createElement('div');
-        this.calendarBody.classList.add('calendar-body');
+    buildBody() {
+        let calendarBody = Bulma.createElement('div', 'calendar-body');
 
-        let daysInMonth = this.monthDays[this.now.getMonth()];
-        let monthStartsOnDay = new Date(null, null, 1).getDay();
-        let monthEndsOnDay = new Date(null, null, daysInMonth).getDay();
+        let daysInMonth = monthDays[this.now.getMonth()];
+        let monthStartsOnDay = new Date(this.year, this.month, 1).getDay();
+        let monthEndsOnDay = new Date(this.year, this.month, daysInMonth).getDay();
 
         let startAt = -monthStartsOnDay + 1;
         let endAt = daysInMonth + monthEndsOnDay + 1;
+        
+        if(endAt > 32) {
+            while (endAt > 32) {
+                endAt--;
+            }
+        }
 
         let cells = [];
 
         for(let i = startAt; i < endAt; i++) {
-            let d = new Date(this.year, this.now.getMonth(), i);
+            let d = new Date(this.year, this.month, i);
 
             let today = false;
             let thisMonth = false;
 
-            if(d.getFullYear() === this.year && d.getMonth() === this.month && d.getDate() === this.day) {
+            if(d.getFullYear() === this.now.getFullYear() && d.getMonth() === this.now.getMonth() && d.getDate() === this.now.getDate()) {
                 today = true;
             }
 
@@ -134,15 +122,13 @@ class Calendar {
         }
 
         cells.forEach((day) => {
-            let d = document.createElement('div');
-            d.classList.add('calendar-date');
+            let d = Bulma.createElement('div', 'calendar-date');
 
             if(!day.isThisMonth) {
                 d.classList.add('is-disabled');
             }
 
-            let button = document.createElement('button');
-            button.classList.add('date-item');
+            let button = Bulma.createElement('button', 'date-item');
 
             if(day.isToday) {
                 button.classList.add('is-today');
@@ -152,28 +138,44 @@ class Calendar {
 
             d.appendChild(button);
 
-            this.calendarBody.appendChild(d);
+            calendarBody.appendChild(d);
         });
+
+        return calendarBody;
+    }
+
+    clearCalendar() {
+        while (this.root.firstChild) {
+            this.root.removeChild(this.root.firstChild);
+        }
     }
 
     render() {
-        this.root.appendChild(this.nav);
+        this.clearCalendar();
 
-        this.calendarContainer.appendChild(this.calendarHeader);
-        this.calendarContainer.appendChild(this.calendarBody);
+        this.root.appendChild(this.buildNav());
 
-        this.root.appendChild(this.calendarContainer);
+        let container = this.buildContainer();
+        container.appendChild(this.buildHeader());
+        container.appendChild(this.buildBody());
+
+        this.root.appendChild(container);
+    }
+
+    /**
+     * Helper method used by the Bulma core to create a new instance.
+     * @param  {Object} options
+     * @return {Calendar}
+     */
+    static create(options) {
+        return new Calendar(options);
     }
 
     /**
      * Handle parsing the DOMs data attribute API.
      */
     static handleDomParsing(element) {
-        let test = new Calendar({
-            element: element
-        });
-
-        console.log(test);
+        return;
     }
 }
 
