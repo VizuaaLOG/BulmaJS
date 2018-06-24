4bfb474354ecf5348e772199914b5fdeb7f80380
Thomas Erbe <vizuaalog@gmail.com>
2018-06-24 22:41:09 +0100

    Layout changes, restructure and nav menu

--- docs/assets/js/docs.js
+++ docs/assets/js/docs.js
@@ -7,3 +7,42 @@ document.addEventListener('scroll', function(event) {
         menu.classList.remove('is-scroll');
     }
 });
+
+var menuItems = [];
+
+for(var i = 0; i < window.docsMenu.length; i++) {
+    menuItems.push({
+        title: window.docsMenu[i][0],
+        url: window.docsMenu[i][1],
+        section: window.docsMenu[i][2],
+        version: window.docsMenu[i][3],
+        sort: window.docsMenu[i][4]
+    });
+}
+
+menuItems = _.sortBy(menuItems, 'sort');
+menuItems = _.groupBy(menuItems, 'version');
+
+menuItems = _.transform(menuItems, function(result, value, key) {
+    result[key] = _.groupBy(value, 'section');
+}, {});
+
+var vue = new Vue({
+    el: '#docs-menu',
+    data: {
+        items: menuItems
+    },
+    computed: {
+        version: function() {
+            return window.defaultVersion;
+        },
+
+        versionItems: function() {
+            return this.items[this.version];
+        },
+
+        currPath: function() {
+            return window.location.pathname;
+        }
+    }
+});
