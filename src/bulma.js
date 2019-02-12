0bb1ad68350cf22a1018f8554e7b26d4085ca4bd
Thomas Erbe <vizuaalog@gmail.com>
2019-02-12 22:48:55 +0000

    [WIP] Alert dialog #47

--- src/bulma.js
+++ src/bulma.js
@@ -6,6 +6,7 @@ import Navbar from './plugins/navbar';
 import Message from './plugins/message';
 import Dropdown from './plugins/dropdown';
 import Modal from './plugins/modal';
+import Alert from './plugins/alert';
 import File from './plugins/file';
 import Tabs from './plugins/tabs';
 
